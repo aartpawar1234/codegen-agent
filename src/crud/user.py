10bef16e2e1a70ea5a88e4851d3516ac44615f34
aartpawar1234 <43437243+aartpawar1234@users.noreply.github.com>
2026-06-29 23:41:02 +0530

    feat: add src\crud\user.py

---
+++ src/crud/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+from ..utils.security import get_password_hash
+
+
+def get_user_by_email(db: Session, email: str) -> models.User:
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+    db_user = models.User(email=user.email, hashed_password=get_password_hash(user.password))
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user

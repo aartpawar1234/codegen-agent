06176ace7079c177c2a18ddece0b474dc480c667
aartpawar1234 <43437243+aartpawar1234@users.noreply.github.com>
2026-06-29 23:40:59 +0530

    feat: add src\config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
+import os
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///./todos.db"
+    database_url: str
+    jwt_secret: str
+    jwt_algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_minutes: int = 60*24
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
 
 settings = Settings()

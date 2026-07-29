7f265e097bdcc1bb002971ebfd9636d9de187bcf
aartpawar1234 <43437243+aartpawar1234@users.noreply.github.com>
2026-07-29 16:19:24 +0530

    feat: add src\database.py

--- src/database.py
+++ src/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings
 
-engine = create_engine(settings.database_url)
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()

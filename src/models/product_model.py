955991ec87e3b3b9c76d6255d6fe0706b3893a43
aartpawar1234 <43437243+aartpawar1234@users.noreply.github.com>
2026-07-29 16:19:32 +0530

    feat: add src\models\product_model.py

---
+++ src/models/product_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from src.database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    stock_quantity = Column(Integer)
+    category = Column(String)
